fdcc27f71239adb3d611991a8062f794203a0460
Hongbeubeu <44913755+Hongbeubeu@users.noreply.github.com>
2023-04-23 14:13:45 +0700

    Add Easy Mobile

---
+++ Assets/Test/Scripts/Concrete.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Test.Scripts
+{
+    public class Concrete : MonoBehaviour
+    {
+        void test()
+        {
+            Test.Instance.haha();
+        }
+    }
+}
---
+++ Assets/Test/Scripts/Test.cs
@@ -0,0 +1,15 @@
+using Ultimate.Core.Runtime.Singleton;
+
+namespace Test.Scripts
+{
+    public class Test : Singleton<Test>
+    {
+        public override void Init()
+        {
+        }
+
+        public void haha()
+        {
+        }
+    }
+}
